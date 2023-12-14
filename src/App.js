9632d4625e43515f54c55c475e377d5ba790b96c
Christine Mathews <34720216+ChristineMathews@users.noreply.github.com>
2023-12-14 13:10:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const birthdayDate = new Date('December 23, 2023 00:00:00 GMT');
+
+  const [countdown, setCountdown] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  const [showCountdown, setShowCountdown] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentDate = new Date();
+      const timeLeft = birthdayDate - currentDate;
+
+      if (timeLeft <= 0) {
+        // Birthday has passed, show the next year countdown
+        setShowCountdown(true);
+        const nextYear = birthdayDate.getFullYear() + 1;
+        birthdayDate.setFullYear(nextYear);
+      }
+
+      const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+      setCountdown({ days, hours, minutes, seconds });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [birthdayDate]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Happy Birthday Celebration!</h1>
+      {showCountdown ? (
+        <div id="countdown">
+          <p>Countdown to the birthday:</p>
+          <p>{`${countdown.days}d ${countdown.hours}h ${countdown.minutes}m ${countdown.seconds}s`}</p>
+        </div>
+      ) : (
+        <div id="greeting">
+          <p>Happy Birthday to the biggest fan of her own birthday!</p>
+          {/* Add more greetings or custom content */}
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
